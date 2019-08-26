4c3e4b2ec0cbfe6f1f439e774182147aa1a2ad7f
gitReiko <khornau@gmail.com>
2019-08-26 15:20:18 +0300

    NeedToCheckTeacherGUI implementation.

--- classes/teacher_gui.php
+++ classes/teacher_gui.php
@@ -12,11 +12,51 @@ class NeedToCheckTeacherGUI
 
     public function get_gui()
     {
-        print_r($this->courses);
-        return "I'm work!!!";
+        $gui = '';
+
+        foreach($this->courses as $course)
+        {
+            $gui.= $this->get_course_row($course);
+
+            foreach($course->get_teachers() as $teacher)
+            {
+                foreach($teacher->get_items() as $item)
+                {
+                    $gui.= $this->get_item_row($item);
+                }
+            }
+        }
+
+        return $gui;
     }
 
+    private function get_course_row($course) : string 
+    {
+        $row = '<div>';
+        $row.= $course->get_name();
+        $row.= $this->get_unchecked_and_expired_string($course);
+        $row.= '</div>';
+        return $row;
+    }
 
+    private function get_unchecked_and_expired_string($value) : string 
+    {
+        // (xx - xx) - xx - works count.
+        $str = ' ('.$value->get_unchecked_works_count().' - ';
+        $str.= '<span style="color: red;">'.$value->get_expired_works_count().'</span>)';
+        return $str;
+    }
+
+    private function get_item_row($item) : string 
+    {
+        $row = '<a href="'.$item->get_link().'" target="_blank">';
+        $row.= '<div style="margin-left: 20px !important;">';
+        $row.= $item->get_name();
+        $row.= $this->get_unchecked_and_expired_string($item);
+        $row.= '</div>';
+        $row.= '</a>';
+        return $row;
+    }
 
 
 }
