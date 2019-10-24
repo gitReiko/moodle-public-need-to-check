c9b8fa546cbb6cac44636d1f42c2da13bd9081a9
Denis Makouski (Reiko) <khornau@gmail.com>
2019-10-24 10:07:36 +0300

    Text strings improvement.

--- lang/en/block_need_to_check.php
+++ lang/en/block_need_to_check.php
@@ -9,17 +9,12 @@ $string['works_for_teachers'] = 'Works for teachers';
 $string['my_works'] = 'My works';
 $string['email'] = 'email';
 $string['phone'] = 'phone';
-$string['time_for_check_work'] = 'Time for check work (timestamp)';
+$string['time_for_check_work'] = 'Time after which work check will be considered overdue (timestamp)';
 $string['time_for_check_work_desc'] = 'After check time expires, ungraded work will be considered expired and displayed in red.<br>Time format is timestamp.<br>The default value is 518400 (6 days).';
-$string['go_to_course'] = 'to course';
-$string['update_teacher_table'] = 'Update teachers table';
-$string['update_teacher_table_title'] = 'Teachers table is necessary for the quick work of the block.&#013;For correct work of block, the table needs to be updated after all changes of teachers checking activity.&#013;The teacher is considered to be checking if he is enrolled in the same group as the student, and also has a role based on the archetype of teacher or editingteacher in the corresponding activity or course.';
-$string['table_updated'] = 'Table updated.';
-
-
-
-
-
+$string['go_to_course'] = 'go to course';
+$string['update_teacher_table'] = 'Update block database';
+$string['update_teacher_table_title'] = 'Updating the block database is necessary for correct work.&#013;&#013;Updating the database needs to be done after changing the composition of the teachers checking students works.&#013;&#013;The teacher is considered to be checking if he is enrolled in the same group as the student, and also has a role based on the archetype of teacher or editingteacher in the corresponding activity or course.&#013;&#013;This block uses a database to increase speed of work.';
+$string['table_updated'] = 'The block table has been updated in the database.';
 
 
 
