4458740b1efc3812f5928a70678bba763e2a2d91
Denis Makouski (Reiko) <khornau@gmail.com>
2019-10-28 15:01:55 +0300

    english text improvement

--- lang/en/block_need_to_check.php
+++ lang/en/block_need_to_check.php
@@ -2,8 +2,8 @@
 
 $string['pluginname'] = 'Need to check block';
 $string['need_to_check'] = 'Need to check';
-$string['need_to_check:addinstance'] = 'Add a new need to check block';
-$string['need_to_check:myaddinstance'] = 'Add a new need to check block to the My Moodle page';
+$string['need_to_check:addinstance'] = 'Add a new «Need to check» block';
+$string['need_to_check:myaddinstance'] = 'Add a new «Need to check» block to the My Moodle page';
 $string['not_assigned'] = 'Not assigned';
 $string['works_for_teachers'] = 'Works for teachers';
 $string['my_works'] = 'My works';
@@ -13,8 +13,8 @@ $string['time_for_check_work'] = 'Time after which work check will be considered
 $string['time_for_check_work_desc'] = 'After check time expires, ungraded work will be considered expired and displayed in red.<br>Time format is timestamp.<br>The default value is 518400 (6 days).';
 $string['go_to_course'] = 'go to course';
 $string['update_teacher_table'] = 'Update block database';
-$string['update_teacher_table_title'] = 'Updating the block database is necessary for correct work.&#013;&#013;Updating the database needs to be done after changing the composition of the teachers checking students works.&#013;&#013;The teacher is considered to be checking if he is enrolled in the same group as the student, and also has a role based on the archetype of teacher or editingteacher in the corresponding activity or course.&#013;&#013;This block uses a database to increase speed of work.';
-$string['table_updated'] = 'The block table has been updated in the database.';
+$string['update_teacher_table_title'] = 'Updating the block database is necessary for correct work.&#013;&#013;Updating the database needs to be done after changing composition of teachers who check students\' works.&#013;&#013;The teacher is considered checking if he is enrolled in the same group as the student, and also has a role based on the archetype of teacher or editingteacher in the corresponding activity or course.&#013;&#013;This block uses database to increase speed of work.';
+$string['table_updated'] = 'Block database table has been updated.';
 
 
 
